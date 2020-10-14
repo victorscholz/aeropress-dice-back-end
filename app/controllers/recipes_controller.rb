450ba9d03efba44e4d74c6faf5f7e7ebc64bf043
victorscholz <vgscholz@gmail.com>
2020-10-13 20:44:34 -0400

    database created, data seeded

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,38 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes
+  end
+
+  def show
+    recipe = find_recipe
+    render json: { id: recipe.id, dice: recipe.dice }
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    render json: recipe
+  end
+
+  def update
+    recipe = find_recipe
+    recipe.update(recipe_params)
+    render json: recipe
+  end
+
+  def destroy
+    recipe = find_recipe
+    Recipe.destroy(params[:id])
+    render json: recipe
+  end
+
+  private
+
+  def find_recipe
+    recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:dice)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,32 @@
 class UsersController < ApplicationController
   def index
-		users = User.all
-		render json: users
-	end
+    users = User.all
+    render json: users
+  end
 
-	def show
-		user = find_user
-		render json: {
-				id: user.id, name: user.name, buildings: user.recipes
-		       }
-	end
+  def show
+    user = find_user
+    render json: { id: user.id, name: user.name, recipes: user.recipes }
+  end
 
-	# def create
-	# 	user = User.create(user_params)
-	# 	render json: user
-	# end 
+  # def create
+  # 	user = User.create(user_params)
+  # 	render json: user
+  # end
 
-	def update
-		user = find_user
-		user.update(user_params)
-		render json: user
-	end
+  # def update
+  # 	user = find_user
+  # 	user.update(user_params)
+  # 	render json: user
+  # end
 
-	private
+  private
 
-	def find_user
-		user = User.find(params[:id])
-	end
+  def find_user
+    user = User.find(params[:id])
+  end
 
-	def user_params
-		params.require(:user).permit(:name)
-	end
-end
+  # def user_params
+  # 	params.require(:user).permit(:name)
+  # end
 end
