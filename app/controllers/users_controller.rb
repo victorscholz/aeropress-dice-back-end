d446f9044c18935b406b0bc305eb78ad2ca4fe93
victorscholz <vgscholz@gmail.com>
2020-10-13 20:23:00 -0400

    setup database

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+		users = User.all
+		render json: users
+	end
+
+	def show
+		user = find_user
+		render json: {
+				id: user.id, name: user.name, buildings: user.recipes
+		       }
+	end
+
+	# def create
+	# 	user = User.create(user_params)
+	# 	render json: user
+	# end 
+
+	def update
+		user = find_user
+		user.update(user_params)
+		render json: user
+	end
+
+	private
+
+	def find_user
+		user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name)
+	end
+end
+end
